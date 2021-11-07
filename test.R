5a9756a088ae49e1a3cd9ace7b52f8bf7384f50a
NagoyaKensuke <46418599+NagoyaKensuke@users.noreply.github.com>
2021-11-07 18:39:52 +0900

    Update test.R

--- test.R
+++ test.R
@@ -13,4 +13,21 @@ x22 <- table(x21$study,x21$result)
 x23 <- prop.table(x22)
 x24 <- addmargins(x23)
 
+#二項分布
 dbinom(4,4,0.8)
+
+# ポアソン分布 事象の発生確率
+dpois(0,2)
+
+#ポアソン分布 n回以下となる確率
+ppois(2,1)
+
+
+
+mean <- 50;sd <- 10;x1 <- 40
+
+y1 <- dnorm(x1,mean=mean,sd=sd)
+
+ggplot(data=data.frame(x=c(0,100)), aes(x=x)) +
+stat_function(fun=dnorm, args=c(mean=mean,sd=sd) ) +
+geom_area(stat = "function", fun = dnorm, args=c(mean=mean,sd=sd),fill = "red", xlim = c(0, x1))
